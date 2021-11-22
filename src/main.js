4e7f1337b8a34ac08f1ac70cf6afb28ff486d9b9
Francisca Villavicencio <franciscavillavicencio@MacBook-Air-de-Francisca.local>
2021-11-22 12:02:34 -0300

    actualización de rutas, template beginning

--- src/main.js
+++ src/main.js
@@ -4,4 +4,7 @@ import { myFunction } from './lib/index.js';
 import { app } from './firebaseConfig.js';
 console.log(app);
 
+import { beginning } from './view/templateBeginning.js';
+
 myFunction();
+document.getElementById('root').innerHTML = beginning()
---
+++ src/view/templateBeginning.js
@@ -0,0 +1,9 @@
+export const beginning = () => {
+  const viewBeginning = `
+
+<button id = "login"  class= "btnLogin">Iniciar Sesión</button>
+<button id = "register"  class = "btnregister">Regístrate</button>
+
+`
+return viewBeginning;
+}
