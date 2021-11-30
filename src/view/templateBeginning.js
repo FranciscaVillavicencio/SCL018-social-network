4fee5f2df4ef5d8fae03ff3104ed52f775c697e6
CarolinaGarciaM <carolina.garciameneses@gmail.com>
2021-11-29 23:51:11 -0300

    Diseño en css

--- src/view/templateBeginning.js
+++ src/view/templateBeginning.js
@@ -2,8 +2,19 @@ export const beginning = () => {
   const viewContainer = document.createElement("div");
   const viewBeginning = `
 
-    <button id = "login"  class = "btnLogin">Iniciar Sesión</button>
-    <button id = "register"  class = "btnregister">Regístrate</button>
+    <header>
+    <div class = "images-container">
+    <img class = "logoFuego" src = "images/logofuego.png" alt = "logo de fuego con carita blanca y sonriente">
+    <img class = "imgPropagar" src = "images/propagar.png" alt = "logo del nombre de la página web, propagar en mayúscula y degradado de naranjos y
+    amarillos con contorno de fuego">
+    </div>
+    </header>
+    <div class = "login-container">
+    <button id = "login"  class = "btnLogin">INICIAR SESIÓN</button>
+    </div>
+    <div class = "register-container">
+    <button id = "register"  class = "btnregister">REGÍSTRATE</button>
+    </div>
 
     `;
   viewContainer.innerHTML = viewBeginning;
--- src/view/templateLogin.js
+++ src/view/templateLogin.js
@@ -2,6 +2,7 @@ export const login = () => {
     const divLogin = document.createElement ("div");
     const viewLogin = `
     
+
     <input id = "emailLogin" class = "login-email" placeholder = "e-mail"></input>
     <input id = "passwordLogin" class = "login-password" placeholder = "password"></input>
     <button id = "btnLogin" class = "login-enter">Enter</button>
