c0de6558ad3285cd1bc7a6b45249a099b68ed899
CarolinaGarciaM <carolina.garciameneses@gmail.com>
2021-12-02 10:57:54 -0300

    Funciones en JavasCript

--- src/view/templateRegister.js
+++ src/view/templateRegister.js
@@ -1,34 +1,29 @@
-import { signUp } from '../firebaseConfig.js';
+import { signUp } from "../firebaseConfig.js";
 
 export const register = () => {
-    const divRegister = document.createElement ("div");
-    const viewRegister =`
+    const divRegister = document.createElement("div");
+    const viewRegister = `
 
     <input id = "nameRegister" class = "register-name"  type="text">
     <input id = "emailRegister" class = "register-email"  type="text">
     <input id = "passwordRegister"  class= "register-password"  type="text">
     <button id= "btnRegister"     class = "register-enter"></button>
     `;
-   
+
     divRegister.innerHTML = viewRegister;
 
-    const buttonRegister = divRegister.querySelector('#btnRegister');
-    buttonRegister.addEventListener('click', () => {
-      const email = divRegister.querySelector('#emailRegister').value;
-      const password = divRegister.querySelector('#passwordRegister').value;
-      console.log(email);
-      console.log(password);
-      if (password.lenght < 6) {
-          alert("tu constraseña es muy corta")//preguntar porque no se activan las alertas//
-      }else{
-          signUp(email,password);
-          
-      }
-     });
+    const buttonRegister = divRegister.querySelector("#btnRegister");
+    buttonRegister.addEventListener("click", () => {
+        const email = divRegister.querySelector("#emailRegister").value;
+        const password = divRegister.querySelector("#passwordRegister").value;
+        console.log(email);
+        console.log(password);
+        if (password.lenght < 6) {
+            alert("tu constraseña es muy corta"); //preguntar porque no se activan las alertas//
+        } else {
+            signUp(email, password);
+        }
+    });
 
-     return divRegister;
-
-   };
-
- 
-
+    return divRegister;
+};
